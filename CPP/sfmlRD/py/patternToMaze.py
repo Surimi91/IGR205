2776c4f2bd40caee00a248db895e8eb496dca04b
servane-desvignes <166547684+servane-desvignes@users.noreply.github.com>
2024-05-26 16:15:52 +0000

    "solvable" maze

--- CPP/sfmlRD/py/patternToMaze.py
+++ CPP/sfmlRD/py/patternToMaze.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
 from skimage.color import label2rgb
 from scipy.spatial import distance
 import networkx as nx
@@ -11,7 +12,7 @@ from skimage.draw import disk
 def load_grid(csv_path):
     return pd.read_csv(csv_path, header=None).values
 
-# Fonction flood fill itératif
+# Fonction flood fill itérative
 def flood_fill_iteratif(grille, x, y, marqueur, marqueurs):
     pile = [(x, y)]
     points = []
@@ -31,48 +32,54 @@ def flood_fill_iteratif(grille, x, y, marqueur, marqueurs):
 
 
 csv_path = 'test.csv'
-grille = 1-load_grid(csv_path)
+grille = load_grid(csv_path)
 
+grille_lissée = gaussian_filter(grille.astype(float), sigma=1)
+grille=grille_lissée
+grille_binaire = (grille_lissée > 0.5).astype(int)
 
-marqueurs = np.zeros_like(grille, dtype=int)
+# Initialiser les marqueurs
+marqueurs = np.zeros_like(grille_binaire, dtype=int)
 marqueur_actuel = 1
 chambres = []
 
-
-
-# Appliquer le flood fill itératif pour chaque pixel blanc non marqué
-for i in range(grille.shape[0]):
-    for j in range(grille.shape[1]):
-        if grille[i, j] == 0 and marqueurs[i, j] == 0:
-            points = flood_fill_iteratif(grille, i, j, marqueur_actuel, marqueurs)
+# flood fill itératif pour chaque pixel blanc non marqué
+for i in range(grille_binaire.shape[0]):
+    for j in range(grille_binaire.shape[1]):
+        if grille_binaire[i, j] == 0 and marqueurs[i, j] == 0:
+            points = flood_fill_iteratif(grille_binaire, i, j, marqueur_actuel, marqueurs)
             centre = np.mean(points, axis=0)
             chambres.append((marqueur_actuel, centre, points))
             marqueur_actuel += 1
 
 
 
-# Visu
-chambres_colorées = np.copy(grille)
+
+
+chambres_colorees = np.copy(grille_binaire)
 for chambre_id, _, points in chambres:
     for (x, y) in points:
-        chambres_colorées[x, y] = chambre_id
+        chambres_colorees[x, y] = chambre_id
 
 
-
+'''
 plt.figure(figsize=(10, 10))
 plt.imshow(chambres_colorées, cmap='tab20')
+plt.title('Grille avec les chambres détectées')
 plt.show()
+'''
 
-# graphe
+
+
 G = nx.Graph()
 
-# noeuds + positions
+#noeuds du graph
 positions = {}
 for chambre_id, centre, _ in chambres:
     positions[chambre_id] = (centre[1], centre[0])
     G.add_node(chambre_id, pos=(centre[1], centre[0]))
 
-# arêtes + proximité directe des chambres
+# Ajouter les arêtes + proximité directe des chambres
 for i in range(len(chambres)):
     id1, centre1, points1 = chambres[i]
     distances = []
@@ -83,45 +90,54 @@ for i in range(len(chambres)):
             distances.append((dist, id2))
 
     distances.sort()
-    for _, neighbor_id in distances[:4]:  
+    for _, neighbor_id in distances[:4]:
         G.add_edge(id1, neighbor_id, weight=_)
 
 
-# D/A
-start_node = random.choice(list(G.nodes))
-end_node = random.choice(list(G.nodes))
-while end_node == start_node:
-    end_node = random.choice(list(G.nodes))
+
+#D/A
+max_dist = 0
+start_node, end_node = None, None
+for i in range(len(chambres)):
+    for j in range(i + 1, len(chambres)):
+        dist = distance.euclidean(chambres[i][1], chambres[j][1])
+        if dist > max_dist:
+            max_dist = dist
+            start_node = chambres[i][0]
+            end_node = chambres[j][0]
+
+
 
 
 shortest_path = nx.dijkstra_path(G, source=start_node, target=end_node)
 
 
-
-plt.figure(figsize=(10, 10))
+'''
+plt.figure(figsize=(6, 6))
 nx.draw(G, pos=positions, with_labels=True, node_color='skyblue', node_size=500, edge_color='gray')
 path_edges = list(zip(shortest_path, shortest_path[1:]))
 nx.draw_networkx_edges(G, pos=positions, edgelist=path_edges, edge_color='red', width=2)
 
 
-
 plt.scatter(*positions[start_node], color='green', s=100, zorder=5)
 plt.scatter(*positions[end_node], color='blue', s=100, zorder=5)
-plt.show()
 
+plt.title('Chemin trouvé dans le graphe des chambres')
+plt.show() '''
 
-
-
+# Superposer le path
 for i in range(len(shortest_path) - 1):
     start = positions[shortest_path[i]]
     end = positions[shortest_path[i + 1]]
 
+
     x1, y1 = int(start[1]), int(start[0])
     x2, y2 = int(end[1]), int(end[0])
 
+
     rr, cc = np.linspace(x1, x2, num=1000, dtype=int), np.linspace(y1, y2, num=1000, dtype=int)
     for (x, y) in zip(rr, cc):
-        rr_disk, cc_disk = disk((x, y), 2) 
+        rr_disk, cc_disk = disk((x, y), 2)
         for (r, c) in zip(rr_disk, cc_disk):
             if 0 <= r < grille.shape[0] and 0 <= c < grille.shape[1]:
                 grille[r, c] = 0
@@ -129,24 +145,16 @@ for i in range(len(shortest_path) - 1):
 
 
 
+# Marquer D/A
 start_pos = positions[start_node]
 end_pos = positions[end_node]
 rr_start, cc_start = disk((int(start_pos[1]), int(start_pos[0])), 2)
 rr_end, cc_end = disk((int(end_pos[1]), int(end_pos[0])), 2)
 
-for r, c in zip(rr_start, cc_start):
-    if 0 <= r < grille.shape[0] and 0 <= c < grille.shape[1]:
-        grille[r, c] = 3 
-for r, c in zip(rr_end, cc_end):
-    if 0 <= r < grille.shape[0] and 0 <= c < grille.shape[1]:
-        grille[r, c] = 4 
 
 
-
-from matplotlib.colors import ListedColormap
-cmap = ListedColormap(['black', 'white', 'red', 'green', 'blue', 'cyan'])
-
-
-plt.figure(figsize=(10, 10))
-plt.imshow(grille, cmap=cmap)
+# final maze display
+plt.figure(figsize=(6, 6))
+grille=1-(grille > 0.5).astype(int)
+plt.imshow(grille, cmap='gray')
 plt.show()
