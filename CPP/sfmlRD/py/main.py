afcacac785ee64cfa5630ee44f6f8e4c127b72c9
servane-desvignes <166547684+servane-desvignes@users.noreply.github.com>
2024-05-28 22:30:40 +0200

    main with multithreading

---
+++ CPP/sfmlRD/py/main.py
@@ -0,0 +1,21 @@
+import subprocess
+import threading
+import time
+
+def run_python(script_path):
+    try:
+        subprocess.run(['python', script_path], check=True)
+        print(f"Executed {script_path} successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to execute {script_path}: {e}")
+
+def run_executable(executable_path):
+    try:
+        subprocess.run([executable_path], check=True)
+        print(f"Executed {executable_path} successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to execute {executable_path}: {e}")
+
+if __name__ == "__main__":
+    gridGeneration = 'py/grid.py'
+    reactionDiffusion = 'bin/app'
